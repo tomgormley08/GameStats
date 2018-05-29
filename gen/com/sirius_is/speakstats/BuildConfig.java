057f6b470d1e23f7210249192071872f09422c7b
tomgormley08 <tomgormley08@gmail.com>
2018-05-29 11:05:13 +0100

    master

---
+++ gen/com/sirius_is/speakstats/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sirius_is.speakstats;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/sirius_is/speakstats/Contact.java
@@ -0,0 +1,55 @@
+package com.sirius_is.speakstats;
+
+public class Contact {
+	
+	//private variables
+	int _id;
+	String _nickname;
+	String _phone_number;
+	
+	// Empty constructor
+	public Contact(){
+		
+	}
+	// constructor
+	public Contact(int id, String nickname, String _phone_number){
+		this._id = id;
+		this._nickname = nickname;
+		this._phone_number = _phone_number;
+	}
+	
+	// constructor
+	public Contact(String nickname, String _phone_number){
+		this._nickname = nickname;
+		this._phone_number = _phone_number;
+	}
+	// getting ID
+	public int getID(){
+		return this._id;
+	}
+	
+	// setting id
+	public void setID(int id){
+		this._id = id;
+	}
+	
+	// getting nickname
+	public String getNickname(){
+		return this._nickname;
+	}
+	
+	// setting nickname
+	public void setNickname(String nickname){
+		this._nickname = nickname;
+	}
+	
+	// getting phone number
+	public String getPhoneNumber(){
+		return this._phone_number;
+	}
+	
+	// setting phone number
+	public void setPhoneNumber(String phone_number){
+		this._phone_number = phone_number;
+	}
+}
